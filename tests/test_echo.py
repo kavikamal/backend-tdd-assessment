ad647332c581d6760d90bdcddb2c35867181a39e
Kavitha Kamal <kavithakamal@gmail.com>
2018-08-14 23:23:56 -0400

    Completed the assesment

--- tests/test_echo.py
+++ tests/test_echo.py
@@ -27,6 +27,22 @@ class TestKatas(unittest.TestCase):
     def test_title(self):
         self.assertEqual(echo.convert_to_title('hEllo worlD'), 'Hello World')
 
+    def test_all_option(self):
+        process = subprocess.Popen(
+            ["python", "./echo.py", "-tul", "heLLo!"],
+            stdout=subprocess.PIPE)
+        stdout, _ = process.communicate()
+        stdout = stdout.strip("\n")
+        self.assertEquals(stdout, "Hello!")
+
+    def test_no_option(self):
+        process = subprocess.Popen(
+            ["python", "./echo.py", "heLLo!"],
+            stdout=subprocess.PIPE)
+        stdout, _ = process.communicate()
+        stdout = stdout.strip("\n")
+        self.assertEquals(stdout, "heLLo!")
+
 
 if __name__ == '__main__':
     unittest.main()
